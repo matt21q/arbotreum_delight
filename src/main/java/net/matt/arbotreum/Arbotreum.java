8b3f435e2c0b331a8c95eed3ca7d2ad144372608
Peruvians Delight try 14 <matyqd10@gmail.com>
2024-09-07 15:29:45 -0500

    New refresh menu

--- src/main/java/net/matt/arbotreum/Arbotreum.java
+++ src/main/java/net/matt/arbotreum/Arbotreum.java
@@ -18,9 +18,9 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(Arbotreum.MOD_ID)
 public class Arbotreum
+
 {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "arbotreum";
